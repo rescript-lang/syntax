rescript-lang/syntax

commit 9278966fda5cb75f7e0f45e35800c7d8ffd67ecc

466: Fix missing js attribute for string in braces (#494)

src/res_core.ml
@@ -2772,7 +2772,7 @@ and parseBracedOrRecordExpr  p =
       Parser.expect Rbrace p;
       expr
     | _ ->
-      let constant = Ast_helper.Exp.constant ~loc:field.loc (Parsetree.Pconst_string(s, None)) in
+      let constant = Ast_helper.Exp.constant ~loc:field.loc (Parsetree.Pconst_string(s, Some("js"))) in
       let a = parsePrimaryExpr ~operand:constant p in
       let e = parseBinaryExpr ~a p 1 in
       let e = parseTernaryExpr e p in

commit aa173f426161bcef4babc21d2cc158a0d1eb1387

Fix problem with string in braces introduced in #494

src/res_core.ml
@@ -2772,7 +2772,8 @@ and parseBracedOrRecordExpr  p =
       Parser.expect Rbrace p;
       expr
     | _ ->
-      let constant = Ast_helper.Exp.constant ~loc:field.loc (Parsetree.Pconst_string(s, Some("js"))) in
+      let tag = if p.mode = ParseForTypeChecker then Some "js" else None in
+      let constant = Ast_helper.Exp.constant ~loc:field.loc (Parsetree.Pconst_string(s, tag)) in
       let a = parsePrimaryExpr ~operand:constant p in
       let e = parseBinaryExpr ~a p 1 in
       let e = parseTernaryExpr e p in

tests/printer/expr/switch.res
@@ -19,3 +19,7 @@ let rec updateSum = (node, ~delta) =>
 let x = @attr switch x {
 | Universe => ()
 }
+
+switch count {
+| 1 => { "one" }
+}