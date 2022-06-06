269e8bd4003294a22000801b2556d39c60043b48
Christoph Knittel <ck@cca.io>
2022-06-06 17:19:13 +0200

    Cleanup: get rid of lib dir

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,6 +1,9 @@
 #!/bin/zsh
 
 # pack and parse the whole codebase using the compiler itself. Kind of a test
-ocaml unix.cma ./scripts/bspack.ml -bs-main Res_cli -I cli -I src -o ./lib/rescript.ml
-rescript ./lib/rescript.ml > ./lib/rescript.res
-ocamlopt.opt -w a -pp "rescript -print binary" -O2 -o rescript -I +compiler-libs ocamlcommon.cmxa -I lib -impl ./lib/rescript.res
+rm -rf ./bootstrap
+mkdir ./bootstrap
+
+ocaml unix.cma ./scripts/bspack.ml -bs-main Res_cli -I cli -I src -o ./bootstrap/rescript.ml
+rescript ./bootstrap/rescript.ml > ./bootstrap/rescript.res
+ocamlopt.opt -w a -pp "rescript -print binary" -O2 -o rescript -I +compiler-libs ocamlcommon.cmxa -I lib -impl ./bootstrap/rescript.res
