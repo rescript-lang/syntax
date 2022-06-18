766d61d6684ffbb020077dad19366b217bd452a2
Christoph Knittel <ck@cca.io>
2022-06-18 07:57:58 +0200

    Reformat sources.

--- src/res_comment.mli
+++ src/res_comment.mli
@@ -1,19 +1,19 @@
 type t
 
-val toString: t -> string
+val toString : t -> string
 
-val loc: t -> Location.t
-val txt: t -> string
-val prevTokEndPos: t -> Lexing.position
+val loc : t -> Location.t
+val txt : t -> string
+val prevTokEndPos : t -> Lexing.position
 
-val setPrevTokEndPos: t -> Lexing.position -> unit
+val setPrevTokEndPos : t -> Lexing.position -> unit
 
-val isDocComment: t -> bool
+val isDocComment : t -> bool
 
-val isSingleLineComment: t -> bool
+val isSingleLineComment : t -> bool
 
-val makeSingleLineComment: loc:Location.t -> string -> t
-val makeMultiLineComment: loc:Location.t -> docComment:bool -> string -> t
-val fromOcamlComment:
+val makeSingleLineComment : loc:Location.t -> string -> t
+val makeMultiLineComment : loc:Location.t -> docComment:bool -> string -> t
+val fromOcamlComment :
   loc:Location.t -> txt:string -> prevTokEndPos:Lexing.position -> t
-val trimSpaces: string -> string
+val trimSpaces : string -> string
