67fec537284579e58bee8e70120bff8e03688fa8
Christoph Knittel <ck@cca.io>
2022-06-18 08:18:01 +0200

    Format sources with ocamlformat and enforce formatting in CI (#565)
    * Enforce formatting in CI
    
    * Reformat sources.

--- src/res_comment.ml
+++ src/res_comment.ml
@@ -1,9 +1,7 @@
-type style =
-  | SingleLine
-  | MultiLine
-  | DocComment
+type style = SingleLine | MultiLine | DocComment
 
-let styleToString s = match s with
+let styleToString s =
+  match s with
   | SingleLine -> "SingleLine"
   | MultiLine -> "MultiLine"
   | DocComment -> "DocComment"
@@ -19,8 +17,7 @@ let loc t = t.loc
 let txt t = t.txt
 let prevTokEndPos t = t.prevTokEndPos
 
-let setPrevTokEndPos t pos =
-  t.prevTokEndPos <- pos
+let setPrevTokEndPos t pos = t.prevTokEndPos <- pos
 
 let isSingleLineComment t = t.style = SingleLine
 
@@ -28,50 +25,38 @@ let isDocComment t = t.style = DocComment
 
 let toString t =
   let {Location.loc_start; loc_end} = t.loc in
-  Format.sprintf
-    "(txt: %s\nstyle: %s\nlocation: %d,%d-%d,%d)"
-    t.txt
-    (styleToString t.style)
-    loc_start.pos_lnum
+  Format.sprintf "(txt: %s\nstyle: %s\nlocation: %d,%d-%d,%d)" t.txt
+    (styleToString t.style) loc_start.pos_lnum
     (loc_start.pos_cnum - loc_start.pos_bol)
     loc_end.pos_lnum
     (loc_end.pos_cnum - loc_end.pos_bol)
 
-let makeSingleLineComment ~loc txt = {
-  txt;
-  loc;
-  style = SingleLine;
-  prevTokEndPos = Lexing.dummy_pos;
-}
+let makeSingleLineComment ~loc txt =
+  {txt; loc; style = SingleLine; prevTokEndPos = Lexing.dummy_pos}
 
-let makeMultiLineComment ~loc ~docComment txt = {
-  txt;
-  loc;
-  style = if docComment then DocComment else MultiLine;
-  prevTokEndPos = Lexing.dummy_pos;
-}
+let makeMultiLineComment ~loc ~docComment txt =
+  {
+    txt;
+    loc;
+    style = (if docComment then DocComment else MultiLine);
+    prevTokEndPos = Lexing.dummy_pos;
+  }
 
-let fromOcamlComment ~loc ~txt ~prevTokEndPos = {
-  txt;
-  loc;
-  style = MultiLine;
-  prevTokEndPos = prevTokEndPos
-}
+let fromOcamlComment ~loc ~txt ~prevTokEndPos =
+  {txt; loc; style = MultiLine; prevTokEndPos}
 
 let trimSpaces s =
   let len = String.length s in
   if len = 0 then s
-  else if String.unsafe_get s 0 = ' ' || String.unsafe_get s (len - 1) = ' ' then (
+  else if String.unsafe_get s 0 = ' ' || String.unsafe_get s (len - 1) = ' '
+  then (
     let i = ref 0 in
-    while !i < len && (String.unsafe_get s !i) = ' ' do
+    while !i < len && String.unsafe_get s !i = ' ' do
       incr i
     done;
     let j = ref (len - 1) in
-    while !j >= !i && (String.unsafe_get s !j) = ' ' do
+    while !j >= !i && String.unsafe_get s !j = ' ' do
       decr j
     done;
-    if !j >= !i then
-      (String.sub [@doesNotRaise]) s !i (!j - !i + 1)
-    else
-      ""
-  ) else s
+    if !j >= !i then (String.sub [@doesNotRaise]) s !i (!j - !i + 1) else "")
+  else s
--- testrunner/res_test.ml
+++ testrunner/res_test.ml
@@ -5,55 +5,53 @@ let () =
   let filename = "./tests/api/resSyntax.res" in
   let prettySource = Res_multi_printer.print `res ~input:filename in
   assert (
-    prettySource = {|// test file
+    prettySource
+    = {|// test file
 
 if true {
   Js.log("true")
 } else {
   Js.log("false")
 }
-|}
-  )
+|})
 
 (* test printing of .resi file*)
 let () =
   let filename = "./tests/api/resiSyntax.resi" in
   let prettySource = Res_multi_printer.print `res ~input:filename in
-  assert (
-    prettySource = {|// test interface file
+  assert (prettySource = {|// test interface file
 
 let x: int
-|}
-  )
+|})
 
 (* test printing of ocaml .ml file *)
 let () =
   let filename = "./tests/api/mlSyntax.ml" in
   let prettySource = Res_multi_printer.print `ml ~input:filename in
   assert (
-    prettySource = {|/* test ml file */
+    prettySource
+    = {|/* test ml file */
 
 let () = print_endline("hello world")
 
 let unicode = "🙈 😅 🙌"
 
 let d = `Sehr Schön`
-|}
-  )
+|})
 
 (* test printing of ocaml .mli file *)
 let () =
   let filename = "./tests/api/mliSyntax.mli" in
   let prettySource = Res_multi_printer.print `ml ~input:filename in
   assert (
-    prettySource = {|/* test mli file */
+    prettySource
+    = {|/* test mli file */
 
 let x: int
 
 /* comment */
 let y: float
-|}
-  )
+|})
 
 let () = print_endline "✅ multi printer api tests"
 
@@ -64,30 +62,31 @@ module OutcomePrinterTests = struct
     Compmisc.init_path false;
     Clflags.nopervasives := true;
     let env = Compmisc.initial_env () in
-    try (
-      let (_typedStructure, signature, _newenv) =
-        Typemod.type_toplevel_phrase env structure in
-      signature
-      |> Printtyp.tree_of_signature
-      |> (!Oprint.out_signature) Format.str_formatter;
-      Format.flush_str_formatter()
-    ) with
+    try
+      let _typedStructure, signature, _newenv =
+        Typemod.type_toplevel_phrase env structure
+      in
+      signature |> Printtyp.tree_of_signature
+      |> !Oprint.out_signature Format.str_formatter;
+      Format.flush_str_formatter ()
+    with
     | Typetexp.Error (_, _, err) ->
       Typetexp.report_error env Format.str_formatter err;
       prerr_string (Format.flush_str_formatter ());
-      exit 1;
+      exit 1
     | Typemod.Error (_, _, err) ->
       Typemod.report_error env Format.str_formatter err;
       prerr_string (Format.flush_str_formatter ());
-      exit 1;
+      exit 1
     | Typedecl.Error (_, err) ->
       Typedecl.report_error Format.str_formatter err;
       prerr_string (Format.flush_str_formatter ());
-      exit 1;
+      exit 1
     | e ->
       prerr_string
-        ("Unknown error while trying to print outcome tree.\n" ^
-        "We don't display all the outcome type errors; try adding the new case to the `try` pattern match.\n");
+        ("Unknown error while trying to print outcome tree.\n"
+       ^ "We don't display all the outcome type errors; try adding the new \
+          case to the `try` pattern match.\n");
       raise e
 
   (* `tests/oprint/oprint.res` will be read into memory and typechecked.
@@ -97,23 +96,24 @@ module OutcomePrinterTests = struct
    * and stored in a snapshot `tests/oprint/expected/oprint.resi.txt` *)
   let run () =
     let filename = "tests/oprint/oprint.res" in
-    let result = Res_driver.parsingEngine.parseImplementation ~forPrinter:false ~filename in
-    let signature = if result.Res_driver.invalid then (
-      Res_driver.parsingEngine.stringOfDiagnostics
-        ~source:(result.source)
-        ~filename:result.filename
-        result.diagnostics;
-      exit 1
-    ) else
-      result.Res_driver.parsetree
+    let result =
+      Res_driver.parsingEngine.parseImplementation ~forPrinter:false ~filename
     in
-    IO.writeFile ~filename:"tests/oprint/expected/oprint.resi.txt" ~contents:(signatureToOutcome signature)
+    let signature =
+      if result.Res_driver.invalid then (
+        Res_driver.parsingEngine.stringOfDiagnostics ~source:result.source
+          ~filename:result.filename result.diagnostics;
+        exit 1)
+      else result.Res_driver.parsetree
+    in
+    IO.writeFile ~filename:"tests/oprint/expected/oprint.resi.txt"
+      ~contents:(signatureToOutcome signature)
 end
 
 module ParserApiTest = struct
   let makeDefault () =
     let src = "   let x = 1\nlet y = 2\nlet z = 3" in
-    let parser = Res_parser.make  src "test.res" in
+    let parser = Res_parser.make src "test.res" in
     assert (parser.scanner.lnum == 1);
     assert (parser.scanner.lineOffset == 0);
     assert (parser.scanner.offset == 6);
@@ -143,11 +143,11 @@ module ParserApiTest = struct
     print_endline "✅ Parser handles CRLF correct"
 
   let run () =
-    makeDefault();
-    unixLf();
-    windowsCrlf()
+    makeDefault ();
+    unixLf ();
+    windowsCrlf ()
 end
 
-let () = OutcomePrinterTests.run()
-let () = ParserApiTest.run()
-let () = Res_utf8_test.run()
+let () = OutcomePrinterTests.run ()
+let () = ParserApiTest.run ()
+let () = Res_utf8_test.run ()
