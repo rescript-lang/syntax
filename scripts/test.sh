fed1a2773915e699216f8b2befa9a71e793a6f82
Christoph Knittel <ck@cca.io>
2022-06-24 21:02:07 +0200

    Fix Windows build and beautify + format ci.yml (#580)
    * ocaml/setup-ocaml@v2.0.3 -> ocaml/setup-ocaml@v2
    
    * Remove setting default repo
    
    * Use matrix, not env for compiler version
    
    * Format ci.yml
    
    * Do not run format check on Windows
    
    * Beautify

--- scripts/test.sh
+++ scripts/test.sh
@@ -105,3 +105,16 @@ if [[ $ROUNDTRIP_TEST = 1 ]]; then
 fi
 
 rm -r temp/
+
+# Check format (does not work on Windows)
+case "$(uname -s)" in
+  Darwin|Linux)
+    echo "Checking code formatting..."
+    if dune build @fmt; then
+      printf "${successGreen}✅ Code formatting ok.${reset}\n"
+    else
+      printf "${warningYellow}⚠️ Code formatting failed.${reset}\n"
+      exit 1
+    fi
+    ;;
+esac
