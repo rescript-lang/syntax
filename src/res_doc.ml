5576952929121e733d02afeb37ed43522f0502f3
Cristiano Calcagno <cristianoc@users.noreply.github.com>
2022-06-28 10:16:11 +0200

    Restore wilBreak functionality.

--- src/res_doc.ml
+++ src/res_doc.ml
@@ -115,7 +115,7 @@ let rec willBreak doc =
   | LineBreak (Hard | Literal) | BreakParent | Group {shouldBreak = true} ->
     true
   | Group {doc} | Indent doc -> willBreak doc
-  | CustomLayout {groups = _lazyDoc :: _} -> false
+  | CustomLayout {groups = lazyDoc :: _} -> willBreak (Lazy.force lazyDoc)
   | Concat docs -> List.exists willBreak docs
   | IfBreaks {yes; no} -> willBreak yes || willBreak no
   | _ -> false
