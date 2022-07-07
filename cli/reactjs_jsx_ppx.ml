1cc4efcb3fa0c7874133d269a7c853e6d0050d5a
Woonki Moon <woonki.moon@gmail.com>
2022-07-07 16:57:27 +0900

    clean up of updating jsx config

--- cli/reactjs_jsx_ppx.ml
+++ cli/reactjs_jsx_ppx.ml
@@ -8,9 +8,9 @@ let hasJsxConfigAttrStr {pstr_desc} =
   | _ -> false
 
 let jsxConfigAttributeStr structure =
-  let firstStrItem = try Some (List.hd structure) with Not_found -> None in
-  match firstStrItem with
-  | Some strItem when hasJsxConfigAttrStr strItem -> firstStrItem
+  match structure with
+  | [] -> None
+  | ([strItem] | strItem :: _) when hasJsxConfigAttrStr strItem -> Some strItem
   | _ -> None
 
 let hasJsxConfigAttrSig {psig_desc} =
@@ -19,10 +19,9 @@ let hasJsxConfigAttrSig {psig_desc} =
   | _ -> false
 
 let jsxConfigAttributeSig signature =
-  (* maybe just check the first structure_item? *)
-  let firstSigItem = try Some (List.hd signature) with Not_found -> None in
-  match firstSigItem with
-  | Some sigItem when hasJsxConfigAttrSig sigItem -> firstSigItem
+  match signature with
+  | [] -> None
+  | ([sigItem] | sigItem :: _) when hasJsxConfigAttrSig sigItem -> Some sigItem
   | _ -> None
 
 let getJsxConfig payload =
@@ -35,7 +34,7 @@ let getJsxConfig payload =
          }
         :: _rest)) ->
     recordFields
-  | _ -> raise (Invalid_argument "jsxConfig accepts a record config only")
+  | _ -> []
 
 type configKey = Int | String
 
@@ -61,87 +60,55 @@ let getJsxConfigByKey ~key ~type_ recordFields =
   | [] -> None
   | [v] | v :: _ -> Some v
 
-let transform ~v3 ~v4 ~jsx_version ~jsx_module ~jsx_mode code =
-  match (jsx_version, jsx_module, jsx_mode) with
-  | 3, _, _ -> v3 code
-  | 4, _, "classic" -> v4 ~jsx_mode:"classic" code
-  | 4, _, "automatic" -> v4 ~jsx_mode:"automatic" code
-  | _ -> code
+let getOrDefaultInt ~key ~default payload =
+  getJsxConfig payload
+  |> getJsxConfigByKey ~key ~type_:Int
+  |> Option.map int_of_string_opt
+  |> Option.join |> Option.value ~default
+
+let getOrDefaultString ~key ~default payload =
+  getJsxConfig payload
+  |> getJsxConfigByKey ~key ~type_:String
+  |> Option.value ~default
+
+let updateConfig ~jsx_version ~jsx_module ~jsx_mode payload =
+  let version = getOrDefaultInt ~key:"version" ~default:jsx_version payload in
+  let module_ = getOrDefaultString ~key:"module" ~default:jsx_module payload in
+  let mode = getOrDefaultString ~key:"mode" ~default:jsx_mode payload in
+  (version, module_, mode)
 
 let rewrite_implementation ~jsx_version ~jsx_module ~jsx_mode
     (code : Parsetree.structure) : Parsetree.structure =
-  match jsxConfigAttributeStr code with
-  (* if @@jsxConfig({ .. }) *)
-  | Some _ -> (
-    let _attr_loc, payload =
-      match jsxConfigAttributeStr code with
-      | Some {pstr_desc = Pstr_attribute ({loc}, payload)} -> (loc, Some payload)
-      | _ -> (Location.none, None)
-    in
-    let version =
-      getJsxConfig payload
-      |> getJsxConfigByKey ~key:"version" ~type_:Int
-      |> Option.map int_of_string_opt
-      |> Option.join
-    in
-    let module_ =
-      getJsxConfig payload |> getJsxConfigByKey ~key:"module" ~type_:String
-    in
-    let mode =
-      getJsxConfig payload |> getJsxConfigByKey ~key:"mode" ~type_:String
-    in
-    match (version, module_, mode) with
-    | Some 3, _, _ -> Reactjs_jsx_ppx_v3.rewrite_implementation code
-    | Some 4, _, Some "classic" ->
-      Reactjs_jsx_ppx_v4.rewrite_implementation ~jsx_mode:"classic" code
-    | Some 4, _, Some "automatic" ->
-      Reactjs_jsx_ppx_v4.rewrite_implementation ~jsx_mode:"automatic" code
-    | _ ->
-      raise
-        (Invalid_argument
-           "jsxConfig has options: version = [3, 4], mode = [\"classic\", \
-            \"automatic\"]"))
-  | None ->
-    transform ~v3:Reactjs_jsx_ppx_v3.rewrite_implementation
-      ~v4:Reactjs_jsx_ppx_v4.rewrite_implementation ~jsx_version ~jsx_module
-      ~jsx_mode code
+  let _attr_loc, payload =
+    match jsxConfigAttributeStr code with
+    | Some {pstr_desc = Pstr_attribute ({loc}, payload)} -> (loc, Some payload)
+    | _ -> (Location.none, None)
+  in
+  let version, module_, mode =
+    updateConfig ~jsx_version ~jsx_module ~jsx_mode payload
+  in
+  match (version, module_, mode) with
+  | 3, _, _ -> Reactjs_jsx_ppx_v3.rewrite_implementation code
+  | 4, _, "classic" ->
+    Reactjs_jsx_ppx_v4.rewrite_implementation ~jsx_mode:"classic" code
+  | 4, _, "automatic" ->
+    Reactjs_jsx_ppx_v4.rewrite_implementation ~jsx_mode:"automatic" code
+  | _ -> code
   [@@raises Invalid_argument, Failure]
 
 let rewrite_signature ~jsx_version ~jsx_module ~jsx_mode
     (code : Parsetree.signature) : Parsetree.signature =
-  match jsxConfigAttributeSig code with
-  (* if @@jsxConfig({ .. }) *)
-  | Some _ -> (
-    let _attr_loc, payload =
-      match jsxConfigAttributeSig code with
-      | Some {psig_desc = Psig_attribute ({loc}, payload)} -> (loc, Some payload)
-      | _ -> (Location.none, None)
-    in
-    let version =
-      getJsxConfig payload
-      |> getJsxConfigByKey ~key:"version" ~type_:Int
-      |> Option.map int_of_string_opt
-      |> Option.join
-    in
-    let module_ =
-      getJsxConfig payload |> getJsxConfigByKey ~key:"module" ~type_:String
-    in
-    let mode =
-      getJsxConfig payload |> getJsxConfigByKey ~key:"mode" ~type_:String
-    in
-    match (version, module_, mode) with
-    | Some 3, _, _ -> Reactjs_jsx_ppx_v3.rewrite_signature code
-    | Some 4, _, Some "classic" ->
-      Reactjs_jsx_ppx_v4.rewrite_signature ~jsx_mode code
-    | Some 4, _, Some "automatic" ->
-      Reactjs_jsx_ppx_v4.rewrite_signature ~jsx_mode code
-    | _ ->
-      raise
-        (Invalid_argument
-           "jsxConfig has options: version = [3, 4], mode = [\"classic\", \
-            \"automatic\"]"))
-  | None ->
-    transform ~v3:Reactjs_jsx_ppx_v3.rewrite_signature
-      ~v4:Reactjs_jsx_ppx_v4.rewrite_signature ~jsx_version ~jsx_module
-      ~jsx_mode code
+  let _attr_loc, payload =
+    match jsxConfigAttributeSig code with
+    | Some {psig_desc = Psig_attribute ({loc}, payload)} -> (loc, Some payload)
+    | _ -> (Location.none, None)
+  in
+  let version, module_, mode =
+    updateConfig ~jsx_version ~jsx_module ~jsx_mode payload
+  in
+  match (version, module_, mode) with
+  | 3, _, _ -> Reactjs_jsx_ppx_v3.rewrite_signature code
+  | 4, _, "classic" -> Reactjs_jsx_ppx_v4.rewrite_signature ~jsx_mode code
+  | 4, _, "automatic" -> Reactjs_jsx_ppx_v4.rewrite_signature ~jsx_mode code
+  | _ -> code
   [@@raises Invalid_argument, Failure]
